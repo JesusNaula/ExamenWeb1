7c52d27171b55dbe0ee429ffa2beea69d0cd2549
JesusNaula <daniel.naula@epn.edu.ec>
2017-08-14 19:53:06 -0500

    Creaciuon de la tabla Usuario

---
+++ Pizza/api/models/Usuario.js
@@ -0,0 +1,26 @@
+/**
+ * Usuario.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+    nombreUsuario: {
+      type: 'string'
+    },
+    correo: {
+      type: 'email'
+    },
+    password: {
+      type: 'string'
+    },
+    listCompraPizza: {
+      collection: 'Pizza',
+      via: 'idUsuario'
+    }
+  }
+};
+
